af110ae7a1d533d4699178b66dc02040844f7758
nataliiahaiduk <gaidutsyl@gmail.com>
2023-01-12 22:54:33 -0500

    check ci

--- cypress/PageObjects/HomePage.js
+++ cypress/PageObjects/HomePage.js
@@ -4,8 +4,13 @@ class HomePage {
 
         getLogo: () => cy.get('#img_comp-jixmod45 img'),
         getTitleFirst: () => cy.get('#comp-l5u6mahm3'),
-        getTitleSecond: () => cy.get('div#comp-l5u6mahn4')
+        getTitleSecond: () => cy.get('div#comp-l5u6mahn4'),
+        getTryNowBtn: () => cy.get('span.M3I7Z2')
 
+    };
+
+    clickTryNowBtn () {
+        getTryNowBtn().click();
     };
 
 };
