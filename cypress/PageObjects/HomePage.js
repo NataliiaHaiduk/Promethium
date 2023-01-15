d7c6472411ad07867cd8e139ae2800ce155d4611
NataliiaHaiduk <83046340+NataliiaHaiduk@users.noreply.github.com>
2023-01-15 01:38:53 -0500

    TC3 (#4)
    * TC3
    
    * removed only

--- cypress/PageObjects/HomePage.js
+++ cypress/PageObjects/HomePage.js
@@ -5,12 +5,20 @@ class HomePage {
         getLogo: () => cy.get('#img_comp-jixmod45 img'),
         getTitleFirst: () => cy.get('#comp-l5u6mahm3'),
         getTitleSecond: () => cy.get('div#comp-l5u6mahn4'),
-        getTryNowBtn: () => cy.get('#comp-jixneksf a')
-
+        getTryNowBtn: () => cy.get('#comp-jixneksf a'),
+        getProductMenu: () => cy.get('#comp-jwtopy710label'),
+        getProductDataConnectorsSubMenu: () => cy.get('#comp-jwtopy710 ul a[href="https://www.pm61data.com/promethium-data-connectors"]')
+       
     };
 
     clickTryNowBtn () {
         this.elements.getTryNowBtn().invoke('removeAttr', 'target').click();
+    };
+
+    clickProductDataConnectorsSubMenu () {
+        this.elements.getProductMenu().realHover().then(() => {
+            this.elements.getProductDataConnectorsSubMenu().click({force: true});
+        });
     };
 
 };
